07598da79a0917259df70835aa48a0037fc6a9cd
eugenio-d-tesio <eugenio@pairtreefamily.com>
2022-12-23 09:48:26 -0300

    main completed

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 import 'package:actions_toolkit_dart/core.dart' as core;
 void main(List<String> args) {
-  
+  final whoToGreet = core.getInput(name: 'who-to-greet', options: const core.InputOptions(required: true));
+  print('Hello $whoToGreet');
+  core.setOutput(name: 'time', value: DateTime.now().toString());
 }
